25ef1643156a3b11361e8ac5f533f09bc1b6a1e0
Nick Flores <nick.flores@whiz.pe>
2022-06-08 13:17:16 -0500

    containers?

---
+++ src/components/Pages/home/Services/Services.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+//styles
+import './services.scss';
+//components
+
+const Services = () => {
+	return(
+		<React.Fragment>
+			<section>
+				<div className="container">
+					<div className="row">
+						<div className="col"></div>
+					</div>
+				</div>
+			</section>
+		</React.Fragment>
+	);
+}
+
+export  {Services};
--- src/includes/navbar/Navbar.jsx
+++ src/includes/navbar/Navbar.jsx
@@ -7,23 +7,25 @@ import './navbar.scss';
 const Navbar = () => {
 	return(
 		<React.Fragment>
-			<header id="header">
-				<div className="navbar_container">
-					<div className="logo_container">
-						<Link to="/">
-							<img src="" alt="MedicalDent Logo" className="logo"/>
-						</Link>
+			<header id="header" className="">
+				<div className="container">
+					<div className="navbar_container">
+						<div className="logo_container">
+							<Link to="/">
+								<img src="" alt="MedicalDent Logo" className="logo"/>
+							</Link>
+						</div>
+						<nav>
+							<ul className="nav">
+								<li className="nav_item"><Link to="/nosotros" className="nosotros">Nosotros</Link></li>
+								<li className="nav_item"><Link to="/diagnóstico-en-imagenes" className="diagnostico">Diagnóstico en imagenes<span className="toggler_icon"></span></Link></li>
+								<li className="nav_item"><Link to="/laboratorio-dental-digital" className="laboratorio">Laboratorio Dental Digital<span className="toggler_icon"></span></Link></li>
+								<li className="nav_item"><Link to="/casos-clinicos" className="casos_clinicos">Casos Clínicos</Link></li>
+								<li className="nav_item"><Link to="/contacto" className="contacto">Contacto</Link></li>
+								<li className="nav_item"><Link to="/ingresar" className="ingresar">Ingresar</Link></li>
+							</ul>
+						</nav>
 					</div>
-					<nav>
-						<ul className="nav">
-							<li className="nav_item"><Link to="/nosotros" className="nosotros">Nosotros</Link></li>
-							<li className="nav_item"><Link to="/diagnóstico-en-imagenes" className="diagnostico">Diagnóstico en imagenes<span className="toggler_icon"></span></Link></li>
-							<li className="nav_item"><Link to="/laboratorio-dental-digital" className="laboratorio">Laboratorio Dental Digital<span className="toggler_icon"></span></Link></li>
-							<li className="nav_item"><Link to="/casos-clinicos" className="casos_clinicos">Casos Clínicos</Link></li>
-							<li className="nav_item"><Link to="/contacto" className="contacto">Contacto</Link></li>
-							<li className="nav_item"><Link to="/ingresar" className="ingresar">Ingresar</Link></li>
-						</ul>
-					</nav>
 				</div>
 			</header>
 		</React.Fragment>
