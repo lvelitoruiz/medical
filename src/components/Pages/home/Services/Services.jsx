5bbb34197ebc42dfcb258e6a3a5404ff4ecb30e8
Nick Flores <nick.flores@whiz.pe>
2022-06-08 15:37:57 -0500

    services done and adding testimonials

--- src/components/Pages/home/Services/Services.jsx
+++ src/components/Pages/home/Services/Services.jsx
@@ -2,14 +2,71 @@ import React from "react";
 //styles
 import './services.scss';
 //components
+import { Link } from "gatsby";
 
 const Services = () => {
 	return(
 		<React.Fragment>
-			<section>
+			<section id="home_services">
 				<div className="container">
 					<div className="row">
-						<div className="col"></div>
+						<div className="col">
+
+							<h2 className="title"><span className="emphasis">Nuestros servicios</span></h2>
+
+							<div className="selector_container">
+								<div className="first ">
+									<span className="icon"></span>
+									<p>Diagnóstico <br /> en imágenes</p>
+								</div>
+
+								<div className="second selected">
+									<span className="icon"></span>
+									<p>Laboratorio <br /> dental digital</p>
+								</div>
+
+								<div className="third ">
+									<span className="icon"></span>
+									<p>Insumos <br /> dentales</p>
+								</div>
+							</div>
+
+							<div className="slider_container"> 
+								<div className="card">
+									<div className="card_img">
+										<img src="" alt="" />
+									</div>
+									<p className="card_title">Servicio para <br /> consultorio</p>
+									<Link to="" className="see_more">Ver más <span className="see_more_icon"></span></Link>
+								</div>
+
+								<div className="card">
+									<div className="card_img">
+										<img src="" alt="" />
+									</div>
+									<p className="card_title">Servicio para <br /> laboratorio</p>
+									<Link to="" className="see_more">Ver más <span className="see_more_icon"></span></Link>
+								</div>
+
+								<div className="card">
+									<div className="card_img">
+										<img src="" alt="" />
+									</div>
+									<p className="card_title">Programa tu <br /> orden de trabajo</p>
+									<Link to="" className="see_more">Ver más <span className="see_more_icon"></span></Link>
+								</div>
+
+								<div className="slider_buttons">
+									<div className="left">
+										<span className="icon"> 0 </span>
+									</div>
+									<div className="right">
+										<span className="icon"> 0 </span>
+									</div>
+								</div>
+							</div>
+
+						</div>
 					</div>
 				</div>
 			</section>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Footer } from "../includes/footer/Footer";
 import { Navbar } from "../includes/navbar/Navbar"
 import { Banner } from "../components/Pages/home/Banner/Banner";
+import { Services } from "../components/Pages/home/Services/Services";
 import { Info } from "../components/Pages/home/Info/Info";
 
 const IndexPage = () => {
@@ -11,6 +12,7 @@ const IndexPage = () => {
 		<React.Fragment>
 			<Navbar />
 				<Info />
+				<Services />
 			<Footer />
 		</React.Fragment>
 	);
