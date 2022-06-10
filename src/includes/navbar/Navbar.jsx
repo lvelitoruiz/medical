0d78b04a92448b91a0ee9376bb8e0e5f07af34c6
Nick Flores <nick.flores@whiz.pe>
2022-06-10 09:49:53 -0500

    changes on banner and navbar

--- src/includes/navbar/Navbar.jsx
+++ src/includes/navbar/Navbar.jsx
@@ -9,22 +9,26 @@ const Navbar = () => {
 		<React.Fragment>
 			<header id="header" className="">
 				<div className="container">
-					<div className="navbar_container">
-						<div className="logo_container">
-							<Link to="/">
-								<img src="" alt="MedicalDent Logo" className="logo"/>
-							</Link>
+					<div className="row">
+						<div className="col">
+							<div className="navbar_container">
+								<div className="logo_container">
+									<Link to="/">
+										<img src="" alt="MedicalDent Logo" className="logo"/>
+									</Link>
+								</div>
+								<nav>
+									<ul className="nav">
+										<li className="nav_item"><Link to="/nosotros" className="nosotros">Nosotros</Link></li>
+										<li className="nav_item"><Link to="/diagnóstico-en-imagenes" className="diagnostico">Diagnóstico en imagenes<span className="toggler_icon"></span></Link></li>
+										<li className="nav_item"><Link to="/laboratorio-dental-digital" className="laboratorio">Laboratorio Dental Digital<span className="toggler_icon"></span></Link></li>
+										<li className="nav_item"><Link to="/casos-clinicos" className="casos_clinicos">Casos Clínicos</Link></li>
+										<li className="nav_item"><Link to="/contacto" className="contacto">Contacto</Link></li>
+										<li className="nav_item"><Link to="/ingresar" className="login">Ingresar<span className="icon"></span></Link></li>
+									</ul>
+								</nav>
+							</div>
 						</div>
-						<nav>
-							<ul className="nav">
-								<li className="nav_item"><Link to="/nosotros" className="nosotros">Nosotros</Link></li>
-								<li className="nav_item"><Link to="/diagnóstico-en-imagenes" className="diagnostico">Diagnóstico en imagenes<span className="toggler_icon"></span></Link></li>
-								<li className="nav_item"><Link to="/laboratorio-dental-digital" className="laboratorio">Laboratorio Dental Digital<span className="toggler_icon"></span></Link></li>
-								<li className="nav_item"><Link to="/casos-clinicos" className="casos_clinicos">Casos Clínicos</Link></li>
-								<li className="nav_item"><Link to="/contacto" className="contacto">Contacto</Link></li>
-								<li className="nav_item"><Link to="/ingresar" className="ingresar">Ingresar</Link></li>
-							</ul>
-						</nav>
 					</div>
 				</div>
 			</header>
